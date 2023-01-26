716b6e9bf4a684c031061963886090c81b60bb82
Marina Motelica <marina.motelica@gmail.com>
2023-01-26 18:44:20 +0300

    fixed popup, css, js, added clock

--- app.js
+++ app.js
@@ -7,10 +7,36 @@ document.getElementById('popup').addEventListener("click", function(e)
 }, false) 
     ;
   
-    document.getElementById('order').addEventListener("click", function(e)
+    document.getElementById('popup-form').addEventListener("click", function(e){
+        
+        if(e.target === document.getElementById('popup-form')){
+           document.getElementById("popup-form").style.display = "none"; 
+        }
+    }, false)
+    ;
+    document.getElementById('p-form').addEventListener("submit", function(e)
     {
+        
         e.preventDefault();
+       
+    
+        console.log(document.getElementById('address'));
+        if(document.getElementById('address').value === ""||document.getElementById('address').value.lenght < 10){
+
+            document.getElementById('address').classList.add('red');
+            return;
+        };
+        if(document.getElementById('phone').value === ""||document.getElementById('phone').value.lenght < 7){
+            document.getElementById('phone').classList.add('red');
+            return;
+        };
     
         document.getElementById("popup-form").style.display = "none";
-    }, false) 
-        ;
+    }, false)
+    ;
+    setInterval(() => {
+        const currentDate = new Date().toLocaleString().split(',')[0];
+        const currentTime = new Date().toLocaleString().split(',')[1];
+        document.querySelector(".special-date").innerHTML = currentDate;
+        document.querySelector(".special-time").innerHTML = currentTime;
+    }, 1000);
